732fe9fe174bb894f9a888afba00ef01816d308d
Songxuan Li <lisongxuan0214@gmail.com>
2025-11-30 18:59:00 +0800

    fixed link

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,9 +1,13 @@
-export default {
-  backend: import.meta.env.VITE_BACKEND || "snookerapis.arkady14.fun",
-  backendUrl: import.meta.env.VITE_BACKEND_URL || "https://"+backend,
-  backendWebCalUrl: import.meta.env.VITE_BACKEND_WEBCAL_URL || "webcal://"+backend,
-  defaultLanguage: import.meta.env.VITE_DEFAULT_LANGUAGE || "en",
-  umamiScriptSrc: import.meta.env.VITE_UMAMI_SCRIPT_SRC || "",
-  umamiScriptdata: import.meta.env.VITE_UMAMI_SCRIPT_DATA || "",
-  timezone: import.meta.env.VITE_TIMEZONE || Asia/Shanghai
-};
+export default (() => {
+  const backend = import.meta.env.VITE_BACKEND || "snookerapis.arkady14.fun";
+  
+  return {
+    backend,
+    backendUrl: import.meta.env.VITE_BACKEND_URL || "https://" + backend,
+    backendWebCalUrl: import.meta.env.VITE_BACKEND_WEBCAL_URL || "webcal://" + backend,
+    defaultLanguage: import.meta.env.VITE_DEFAULT_LANGUAGE || "en",
+    umamiScriptSrc: import.meta.env.VITE_UMAMI_SCRIPT_SRC || "",
+    umamiScriptdata: import.meta.env.VITE_UMAMI_SCRIPT_DATA || "",
+    timezone: import.meta.env.VITE_TIMEZONE || "Asia/Shanghai"
+  };
+})();
