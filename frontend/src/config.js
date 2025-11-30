ddddd82d1d8af79975015ba217f9f4bda11e0d46
Songxuan Li <lisongxuan0214@gmail.com>
2025-11-30 18:55:59 +0800

    fixed link

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,7 +1,9 @@
 export default {
-  backendUrl: import.meta.env.VITE_BACKEND_URL || "https://snookerapis.arkady14.fun",
+  backend: import.meta.env.VITE_BACKEND || "snookerapis.arkady14.fun",
+  backendUrl: import.meta.env.VITE_BACKEND_URL || "https://"+backend,
+  backendWebCalUrl: import.meta.env.VITE_BACKEND_WEBCAL_URL || "webcal://"+backend,
   defaultLanguage: import.meta.env.VITE_DEFAULT_LANGUAGE || "en",
   umamiScriptSrc: import.meta.env.VITE_UMAMI_SCRIPT_SRC || "",
   umamiScriptdata: import.meta.env.VITE_UMAMI_SCRIPT_DATA || "",
-  timezone: import.meta.env.VITE_TIMEZONE || 'Asia/Shanghai'
+  timezone: import.meta.env.VITE_TIMEZONE || Asia/Shanghai
 };
